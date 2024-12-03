2b537738b7b217ef87e3a23c9e1b61f5168e812a
alineccorrea <alineccorrea97@gmail.com>
2024-12-02 21:22:29 -0300

    Iniciando projeto SpringBoot

---
+++ todolist/src/test/java/br/com/alineccorrea/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alineccorrea.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
