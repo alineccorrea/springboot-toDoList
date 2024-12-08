fc76f5367667f44820091c88355778fccff7a951
alineccorrea <alineccorrea97@gmail.com>
2024-12-08 16:05:34 -0300

    Validando criação de registro único para o campo username

--- todolist/src/main/java/br/com/alineccorrea/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/alineccorrea/todolist/user/UserController.java
@@ -15,6 +15,13 @@ public class UserController {
 
     @PostMapping("/")
     public UserModel create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            System.out.println("Usuário já existe no banco de dados(User already exists)" + userModel.getUsername());
+            return null;
+        }
+
         var userCreated = this.userRepository.save(userModel);
         return userCreated;
     }
