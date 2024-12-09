e83db808980332edd80153f5e8fdf4899113e4c0
alineccorrea <alineccorrea97@gmail.com>
2024-12-08 21:39:20 -0300

    Implementação do Filter de autenticação na chamada do TaskController

--- todolist/src/main/java/br/com/alineccorrea/todolist/filter/FilterTaskAuth.java
+++ todolist/src/main/java/br/com/alineccorrea/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,12 @@ package br.com.alineccorrea.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.alineccorrea.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,38 +17,57 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter{
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+
         
-        //Pegar usuário e senha
-        var authorization = request.getHeader("Authorization");
-
-        //Removendo string "Basic" do parametro de authorization
-        var authEncoded = authorization.substring("Basic".length()).trim();
-
-        //Decodificar base64
-        byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
-
-        //Transformar em String
-        String authString = new String(authDecoded);
-
-        //Extrair usuário e senha
-        String[] credentials = authString.split(":");
-        var username = credentials[0];
-        var password = credentials[1];
-
-        System.out.println("Authorization:");
-        System.out.println("username: " + username);
-        System.out.println("password: " + password);
-
-        //Validar usuário
-
-        //Validar Senha
-
-        //Segue execução
+        var servletPath = request.getServletPath();
         
-        filterChain.doFilter(request, response);
+        //Executar apenas na rota de tasks
+        if(servletPath.equals("/tasks/")) {
+
+            //Pegar usuário e senha
+            var authorization = request.getHeader("Authorization");
+
+            //Removendo string "Basic" do parametro de authorization
+            var authEncoded = authorization.substring("Basic".length()).trim();
+
+            //Decodificar base64
+            byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+
+            //Transformar em String
+            String authString = new String(authDecoded);
+
+            //Extrair usuário e senha
+            String[] credentials = authString.split(":");
+            var username = credentials[0];
+            var password = credentials[1];
+
+            //Validar usuário
+            var user = this.userRepository.findByUsername(username);
+
+            if(user == null) {
+                response.sendError(401);
+            } else {
+                
+                //Validar Senha
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+                if(passwordVerify.verified) {
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
+
+            }
+        } else {
+            filterChain.doFilter(request, response);
+        }
+        
     }
     
 }
