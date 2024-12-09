ff08636d6152f6a850596fdc6ee3119fdafcf3b8
alineccorrea <alineccorrea97@gmail.com>
2024-12-08 22:39:24 -0300

    Validação de datas e horas das Tasks enviadas

--- todolist/src/main/java/br/com/alineccorrea/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/alineccorrea/todolist/task/TaskController.java
@@ -1,8 +1,11 @@
 package br.com.alineccorrea.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +21,33 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
 
         //setando o idUser capturado e adicionado na request no momento da validação do filter
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
 
+        //Validando datas e horas
+        var currentDate = LocalDateTime.now();
+        
+        //Se a data atual é maior que a data de inicio da task OU
+        //Se a data atual é maior que a data final da task
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("A data de inicio/data de término da task deve ser maior do que a data atual.");
+            //Exemplo NÃO PERMITE
+            /* currentDate 10/12/2024 startAt 10/10/2024 OU
+                currentDate 10/12/2024 endAt 10/10/2024 */
+        }
+
+        //Validando se a data de inicio da task é maior que a data de fim da task
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("A data de inicio deve ser menor que a data de término da task.");
+            /* Exemplo NÃO PERMITE - startAt 10/10/2024 7h endAt 10/05/2024 10h */
+        }
+
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 }
